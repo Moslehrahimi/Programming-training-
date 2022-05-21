a637d9891e09a8adac1cbd74b7f03f51092b4bf0
Moslehkochari <moslehkochari@gmail.com>
2022-05-21 11:49:20 +0430

    install jetstream

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,15 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [Homecontroller::class , 'index']);
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+
+mix.js('resources/js/app.scss', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('postcss-import'),
+        require('tailwindcss'),
     ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
